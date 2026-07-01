168558f0214c4c22c74c063e5da584bfe108d638
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-02 00:29:36 +0530

    keyword arguments

---
+++ 6-keyword args.py
@@ -0,0 +1,9 @@
+#positional arguments
+print("My name is")
+print("Python")
+
+#keyword args
+print("Hello","python", sep="*", end=";")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
