e91693bc1f4bed9ed91e64d28fbb7f1a120b7641
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-04 22:42:37 +0530

    problem02

---
+++ 11-problem02.py
@@ -0,0 +1,9 @@
+#family apples
+#Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. They were all very happy and lived for a long time. End of story.
+
+john = 3
+mary = 5
+adam = 6
+
+print(john,mary,adam,sep=",")
+print("total apples : ", john+mary+adam)
