1143c7b799bb30638502e03021823005064a75b8
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-05 20:15:41 +0530

    operators

---
+++ 18-operators.py
@@ -0,0 +1,18 @@
+#operators
+#   ==(equal-args)
+print(0==1)
+black_sheep == 2*white_sheep
+
+#not equal
+
+var = 0
+print(var != 0)
+
+
+# greater than, greaterthan or equal
+blacksheep > whitesheep
+centigrade_outside >= 0.0
+
+#less than, less than or equal
+current_velocity_mph<85
+current_velocity_mph<=85
