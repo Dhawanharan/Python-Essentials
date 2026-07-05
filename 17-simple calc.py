7f177367c8f8eb0bd7c102596adbadbbe7e5aa61
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-05 20:13:46 +0530

    simple calculator

---
+++ 17-simple calc.py
@@ -0,0 +1,17 @@
+#simple calc
+# input a float value for variable a here
+a = float(input("input a = "))
+# input a float value for variable b here
+b = float(input("input b = "))
+
+# output the result of addition here
+print("a + b = ",a + b)
+# output the result of subtraction here
+print("a - b = ",a - b)
+# output the result of multiplication here
+print("a * b = ",a * b)
+# output the result of division here
+print("a / b = ",a / b)
+
+print("\nThat's all, folks!")
+
