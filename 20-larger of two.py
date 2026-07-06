9cf6d23bb9f3a684ce71964ef174ec13b46633f9
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-06 20:54:14 +0530

    large of two

---
+++ 20-larger of two.py
@@ -0,0 +1,9 @@
+#how to identify the larger of two numbers:
+num1 = int(input("Enter num 1 : "))
+num2 = int(input("Enter num 2 : "))
+
+if num1>num2:
+    larger_num = num1
+else:
+    larger_num = num2
+print("Larger num = ",larger_num)
