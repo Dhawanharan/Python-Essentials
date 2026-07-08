d2694141008030b8bb4224a65fb280d7a8ba6401
Dhawanharan Mahalingam <155748779+Dhawanharan@users.noreply.github.com>
2026-07-08 16:39:16 +0530

    logical operations

---
+++ 31-logical operations.py
@@ -0,0 +1,16 @@
+#logical operations
+var =1
+print(var>0)
+print(not(var<=0))
+
+i =1
+j = not not  i
+print(j)
+
+#bitwise operations
+i = 15
+j = 22
+log = i and j
+bitw = i & j
+print(log)
+print(bitw)
